017beb1d7f43912f8b65a93149683ce2ff962140
nikolaslps <nikolas.lappas.2003@gmail.com>
2026-04-03 17:06:16 +0300

    Adjusted the height

--- hri_body_detect/multibody_detector.py
+++ hri_body_detect/multibody_detector.py
@@ -930,10 +930,10 @@ class SingleBody:
                 self.velocity_pub.publish(self.velocity_msg)
 
         if not self.use_depth:
-            translation = (torso_res[0], 0.9, torso_res[2])
+            translation = (torso_res[0], 0.65, torso_res[2])
         else:
             translation = (self.body_filtered_position[1],
-                           0.9,
+                           0.65,
                            self.body_filtered_position[0])
         self.tb.sendTransform(
             self.create_transform(
